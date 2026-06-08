88e2bf760b518e86984a017327cc72170d3629df
Виктория <viktorianiki@bk.ru>
2026-06-08 15:05:11 +0700

    Update circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <cmath>
 
-// Константа числа π
 const double PI = acos(-1.0);
 
-// Функция для вычисления длины окружности
 double circumference(double radius) {
     return 2 * PI * radius;
 }
 
-// Функция для вычисления площади круга
 double circleArea(double radius) {
     return PI * radius * radius;
 }
 
-// Функция для вычисления площади кругового сектора (угол в градусах)
 double sectorArea(double radius, double angleDegrees) {
     return (PI * radius * radius * angleDegrees) / 360.0;
 }
@@ -22,7 +18,6 @@ double sectorArea(double radius, double angleDegrees) {
 int main() {
     double radius, angle;
 
-    // Ввод и проверка радиуса (должен быть > 0)
     do {
         std::cout << "Введите радиус окружности (больше 0): ";
         std::cin >> radius;
@@ -31,7 +26,6 @@ int main() {
         }
     } while (radius <= 0);
 
-    // Ввод и проверка угла сектора (от 0 до 360 включительно)
     do {
         std::cout << "Введите угол сектора в градусах (от 0 до 360): ";
         std::cin >> angle;
@@ -40,12 +34,10 @@ int main() {
         }
     } while (angle < 0 || angle > 360);
 
-    // Вычисление параметров
     double len = circumference(radius);
     double area_circle = circleArea(radius);
     double area_sector = sectorArea(radius, angle);
 
-    // Вывод результатов
     std::cout << "Длина окружности: " << len << std::endl;
     std::cout << "Площадь круга: " << area_circle << std::endl;
     std::cout << "Площадь кругового сектора (угол " << angle << "°): " << area_sector << std::endl;
